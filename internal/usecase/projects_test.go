183211a3bdd94fca161bbf302ae01c8380767c37
Zakhar <marinin.vsyakoe@gmail.com>
2026-07-28 22:31:25 +0600

    project tests

--- internal/usecase/projects_test.go
+++ internal/usecase/projects_test.go
@@ -1,27 +1,22 @@
 package usecase_test
 
 import (
-	"bernard/internal/config"
 	"bernard/internal/domain/entity"
 	"bernard/internal/usecase"
 	mocks "bernard/internal/usecase/mocks"
+	"errors"
+	"io"
+
 	"bernard/utils"
 	"context"
 	"log/slog"
-	"os"
 	"testing"
 
 	"github.com/google/uuid"
-	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/require"
 )
 
 func TestUseCase_CreateProject(t *testing.T) {
-	type fields struct {
-		log *slog.Logger
-		db  *mocks.MockStorage
-	}
-
 	type args struct {
 		ctx     context.Context
 		project entity.Project
@@ -29,9 +24,9 @@ func TestUseCase_CreateProject(t *testing.T) {
 
 	tests := []struct {
 		name    string
-		fields  fields
 		args    args
-		want    *entity.Project
+		mockSetup func(ms *mocks.MockStorage, mr *mocks.MockRedis, a args)
+		want *entity.Project
 		wantErr bool
 	}{
 		{
@@ -42,6 +37,51 @@ func TestUseCase_CreateProject(t *testing.T) {
 					Name: "test",
 				},
 			},
+			mockSetup: func(ms *mocks.MockStorage, mr *mocks.MockRedis, a args) {
+				ms.On("CreateProject", a.ctx, a.project).
+					Return(&entity.Project{
+						Name: "test",
+					}, nil).Once()
+				mr.On("InvalidateProjectCache", a.ctx, a.project.UserID, a.project.ID).
+					Return(nil).
+					Once()
+			},
+			want: &entity.Project{
+				Name: "test",
+			},
+			wantErr: false,
+		},
+		{
+			name: "bd error",
+			args: args{
+				ctx: context.Background(),
+			},
+			mockSetup: func(ms *mocks.MockStorage, mr *mocks.MockRedis, a args) {
+				ms.On("CreateProject", a.ctx, a.project).
+					Return(nil, errors.New("db error")).
+					Once()
+			},
+			want: nil,
+			wantErr: true,
+		},
+		{
+			name: "redis error",
+			args: args{
+				ctx: context.Background(),
+				project: entity.Project{
+					Name: "test",
+				},
+			},
+			mockSetup: func(ms *mocks.MockStorage, mr *mocks.MockRedis, a args) {
+				ms.On("CreateProject", a.ctx, a.project).
+					Return(&entity.Project{
+						Name: "test",
+					}, nil).
+					Once()
+				mr.On("InvalidateProjectCache", a.ctx, a.project.UserID, a.project.ID).
+					Return(errors.New("redis error")).
+					Once()
+			},
 			want: &entity.Project{
 				Name: "test",
 			},
@@ -51,38 +91,34 @@ func TestUseCase_CreateProject(t *testing.T) {
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			log := slog.New(slog.NewTextHandler(os.Stdout, nil))
-			mockStorage := mocks.NewMockStorage(t)
+			log := slog.New(slog.NewTextHandler(io.Discard, nil))
 
-			mockStorage.On("CreateProject", tt.args.ctx, tt.args.project).Return(tt.want, nil)
+			mockRedis := mocks.NewMockRedis(t)
+			mockStorage := mocks.NewMockStorage(t)
+			if tt.mockSetup != nil {
+				tt.mockSetup(mockStorage, mockRedis, tt.args)
+			}
 
 			u := &usecase.UseCase{
 				Log:    log,
 				DB:     mockStorage,
+				Redis: mockRedis,
 			}
 
 			got, err := u.CreateProject(tt.args.ctx, tt.args.project)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("CreateProject() error = %v, wantErr %v", err, tt.wantErr)
-			}
-
-			if tt.want != nil {
-				require.Equal(t, tt.want, got)
-			}
 
 			if tt.wantErr {
 				require.Error(t, err)
+				require.Nil(t, got)
+			} else {
+				require.NoError(t, err)
+				require.Equal(t, tt.want, got)
 			}
 		})
 	}
 }
 
 func TestUseCase_UpdateProject(t *testing.T) {
-	type fields struct {
-		log *slog.Logger
-		db  *mocks.MockStorage
-	}
-
 	type args struct {
 		ctx       context.Context
 		project   entity.UpdateProjectRequest
@@ -92,8 +128,8 @@ func TestUseCase_UpdateProject(t *testing.T) {
 
 	tests := []struct {
 		name    string
-		fields  fields
 		args    args
+		mockSetup func(ms *mocks.MockStorage, mr *mocks.MockRedis, a args)
 		want    *entity.Project
 		wantErr bool
 	}{
@@ -108,6 +144,61 @@ func TestUseCase_UpdateProject(t *testing.T) {
 				projectID: 1,
 				userID:    uuid.New(),
 			},
+			mockSetup: func(ms *mocks.MockStorage, mr *mocks.MockRedis, a args) {
+				ms.On("UpdateProject", a.ctx, a.project, a.projectID, a.userID).
+					Return(&entity.Project{
+						Name: "test",
+						Description: utils.Ptr("test"),
+					}, nil).
+					Once()
+				mr.On("InvalidateProjectCache", a.ctx, a.userID, a.projectID).
+					Return(nil).
+					Once()
+			},
+			want: &entity.Project{
+				Name:        "test",
+				Description: utils.Ptr("test"),
+			},
+			wantErr: false,
+		},
+		{
+			name: "db error",
+			args: args{
+				ctx: context.Background(),
+				project: entity.UpdateProjectRequest{},
+				projectID: 0,
+				userID: uuid.New(),
+			},
+			mockSetup: func(ms *mocks.MockStorage, mr *mocks.MockRedis, a args) {
+				ms.On("UpdateProject", a.ctx, a.project, a.projectID, a.userID).
+					Return(nil, errors.New("db error")).
+					Once()
+			},
+			want: nil,
+			wantErr: true,
+		},
+		{
+			name: "redis error",
+			args: args{
+				ctx: context.Background(),
+				project: entity.UpdateProjectRequest{
+					Name:        utils.Ptr("test"),
+					Description: utils.Ptr("test"),
+				},
+				projectID: 1,
+				userID:    uuid.New(),
+			},
+			mockSetup: func(ms *mocks.MockStorage, mr *mocks.MockRedis, a args) {
+				ms.On("UpdateProject", a.ctx, a.project, a.projectID, a.userID).
+					Return(&entity.Project{
+						Name: "test",
+						Description: utils.Ptr("test"),
+					}, nil).
+					Once()
+				mr.On("InvalidateProjectCache", a.ctx, a.userID, a.projectID).
+					Return(nil).
+					Once()
+			},
 			want: &entity.Project{
 				Name:        "test",
 				Description: utils.Ptr("test"),
@@ -118,234 +209,235 @@ func TestUseCase_UpdateProject(t *testing.T) {
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			log := slog.New(slog.NewTextHandler(os.Stdout, nil))
+			log := slog.New(slog.NewTextHandler(io.Discard, nil))
+			
 			mockStorage := mocks.NewMockStorage(t)
-
-			mockStorage.On("UpdateProject", tt.args.ctx, tt.args.project).Return(tt.want, nil)
-
+			mockRedis := mocks.NewMockRedis(t)
+			if tt.mockSetup != nil {
+				tt.mockSetup(mockStorage, mockRedis, tt.args)
+			}
+			
 			u := &usecase.UseCase{
 				Log:    log,
 				DB:     mockStorage,
+				Redis: mockRedis,
 			}
 
 			got, err := u.UpdateProject(tt.args.ctx, tt.args.project, tt.args.projectID, tt.args.userID)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("UpdateProject() error = %v, wantErr %v", err, tt.wantErr)
-			}
 
-			if tt.want != nil {
+			if tt.wantErr {
+				require.Error(t, err)
+				require.Nil(t, got)
+			} else {
+				require.NoError(t, err)
 				require.Equal(t, tt.want, got)
 			}
-
-			if tt.wantErr {
-				require.Error(t, err)
-			}
 		})
 	}
 }
 
-func TestUseCase_DeleteProject(t *testing.T) {
-	type fields struct {
-		log *slog.Logger
-		db  *mocks.MockStorage
-	}
+// func TestUseCase_DeleteProject(t *testing.T) {
+// 	type fields struct {
+// 		log *slog.Logger
+// 		db  *mocks.MockStorage
+// 	}
 
-	type args struct {
-		ctx       context.Context
-		projectID int64
-		userID    uuid.UUID
-	}
+// 	type args struct {
+// 		ctx       context.Context
+// 		projectID int64
+// 		userID    uuid.UUID
+// 	}
 
-	tests := []struct {
-		name    string
-		fields  fields
-		args    args
-		want    *entity.Project
-		wantErr bool
-	}{
-		{
-			name: "success",
-			args: args{
-				ctx:       context.Background(),
-				projectID: 1,
-				userID:    uuid.New(),
-			},
-			wantErr: false,
-		},
-		{
-			name: "error",
-			args: args{
-				ctx:       context.Background(),
-				projectID: -1,
-				userID:    uuid.New(),
-			},
-			wantErr: true,
-		},
-	}
+// 	tests := []struct {
+// 		name    string
+// 		fields  fields
+// 		args    args
+// 		want    *entity.Project
+// 		wantErr bool
+// 	}{
+// 		{
+// 			name: "success",
+// 			args: args{
+// 				ctx:       context.Background(),
+// 				projectID: 1,
+// 				userID:    uuid.New(),
+// 			},
+// 			wantErr: false,
+// 		},
+// 		{
+// 			name: "error",
+// 			args: args{
+// 				ctx:       context.Background(),
+// 				projectID: -1,
+// 				userID:    uuid.New(),
+// 			},
+// 			wantErr: true,
+// 		},
+// 	}
 
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			log := slog.New(slog.NewTextHandler(os.Stdout, nil))
-			mockStorage := mocks.NewMockStorage(t)
+// 	for _, tt := range tests {
+// 		t.Run(tt.name, func(t *testing.T) {
+// 			log := slog.New(slog.NewTextHandler(os.Stdout, nil))
+// 			mockStorage := mocks.NewMockStorage(t)
 
-			mockStorage.On("DeleteProject", tt.args.ctx, tt.args.projectID).Maybe().Return(tt.want, nil)
+// 			mockStorage.On("DeleteProject", tt.args.ctx, tt.args.projectID).Maybe().Return(tt.want, nil)
 
-			u := &usecase.UseCase{
-				Log:    log,
-				DB:     mockStorage,
-			}
+// 			u := &usecase.UseCase{
+// 				Log:    log,
+// 				DB:     mockStorage,
+// 			}
 
-			err := u.DeleteProject(tt.args.ctx, tt.args.projectID, tt.args.userID)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("DeleteProject() error = %v, wantErr %v", err, tt.wantErr)
-			}
+// 			err := u.DeleteProject(tt.args.ctx, tt.args.projectID, tt.args.userID)
+// 			if (err != nil) != tt.wantErr {
+// 				t.Errorf("DeleteProject() error = %v, wantErr %v", err, tt.wantErr)
+// 			}
 
-			if tt.wantErr {
-				require.Error(t, err)
-			}
-		})
-	}
-}
+// 			if tt.wantErr {
+// 				require.Error(t, err)
+// 			}
+// 		})
+// 	}
+// }
 
-func TestUseCase_GetProjectTree(t *testing.T) {
-	type fields struct {
-		cfg *config.Config
-		log *slog.Logger
-		db  *mocks.MockStorage
-	}
+// func TestUseCase_GetProjectTree(t *testing.T) {
+// 	type fields struct {
+// 		cfg *config.Config
+// 		log *slog.Logger
+// 		db  *mocks.MockStorage
+// 	}
 
-	type args struct {
-		ctx       context.Context
-		projectID int64
-		userID    uuid.UUID
-		limit     uint64
-		offset    uint64
-	}
+// 	type args struct {
+// 		ctx       context.Context
+// 		projectID int64
+// 		userID    uuid.UUID
+// 		limit     uint64
+// 		offset    uint64
+// 	}
 
-	tests := []struct {
-		name    string
-		fields  fields
-		args    args
-		want    *entity.Project
-		wantErr bool
-	}{
-		{
-			name: "success",
-			args: args{
-				ctx:       context.Background(),
-				projectID: 1,
-				userID:    uuid.New(),
-			},
-			want: &entity.Project{
-				ID: 1,
-			},
-			wantErr: false,
-		},
-		{
-			name: "error",
-			args: args{
-				ctx:       context.Background(),
-				projectID: -1,
-				userID:    uuid.New(),
-			},
-			want:    nil,
-			wantErr: true,
-		},
-	}
+// 	tests := []struct {
+// 		name    string
+// 		fields  fields
+// 		args    args
+// 		want    *entity.Project
+// 		wantErr bool
+// 	}{
+// 		{
+// 			name: "success",
+// 			args: args{
+// 				ctx:       context.Background(),
+// 				projectID: 1,
+// 				userID:    uuid.New(),
+// 			},
+// 			want: &entity.Project{
+// 				ID: 1,
+// 			},
+// 			wantErr: false,
+// 		},
+// 		{
+// 			name: "error",
+// 			args: args{
+// 				ctx:       context.Background(),
+// 				projectID: -1,
+// 				userID:    uuid.New(),
+// 			},
+// 			want:    nil,
+// 			wantErr: true,
+// 		},
+// 	}
 
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			log := slog.New(slog.NewTextHandler(os.Stdout, nil))
-			mockStorage := mocks.NewMockStorage(t)
+// 	for _, tt := range tests {
+// 		t.Run(tt.name, func(t *testing.T) {
+// 			log := slog.New(slog.NewTextHandler(os.Stdout, nil))
+// 			mockStorage := mocks.NewMockStorage(t)
 
-			mockStorage.On("GetProject", tt.args.ctx, tt.args.projectID, mock.Anything).Maybe().Return(tt.want, nil)
+// 			mockStorage.On("GetProject", tt.args.ctx, tt.args.projectID, mock.Anything).Maybe().Return(tt.want, nil)
 
-			u := &usecase.UseCase{
-				Log:    log,
-				DB:     mockStorage,
-			}
+// 			u := &usecase.UseCase{
+// 				Log:    log,
+// 				DB:     mockStorage,
+// 			}
 
-			got, err := u.GetProject(tt.args.ctx, tt.args.projectID, tt.args.userID, tt.args.limit, tt.args.offset)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("GetProjectTree() error = %v, wantErr %v", err, tt.wantErr)
-			}
+// 			got, err := u.GetProject(tt.args.ctx, tt.args.projectID, tt.args.userID, tt.args.limit, tt.args.offset)
+// 			if (err != nil) != tt.wantErr {
+// 				t.Errorf("GetProjectTree() error = %v, wantErr %v", err, tt.wantErr)
+// 			}
 
-			if tt.want != nil {
-				require.Equal(t, tt.want, got)
-			}
+// 			if tt.want != nil {
+// 				require.Equal(t, tt.want, got)
+// 			}
 
-			if tt.wantErr {
-				require.Error(t, err)
-			}
-		})
-	}
-}
+// 			if tt.wantErr {
+// 				require.Error(t, err)
+// 			}
+// 		})
+// 	}
+// }
 
-func TestUseCase_GetProjects(t *testing.T) {
-	type fields struct {
-		cfg *config.Config
-		log *slog.Logger
-		db  *mocks.MockStorage
-	}
+// func TestUseCase_GetProjects(t *testing.T) {
+// 	type fields struct {
+// 		cfg *config.Config
+// 		log *slog.Logger
+// 		db  *mocks.MockStorage
+// 	}
 
-	type args struct {
-		ctx    context.Context
-		userID uuid.UUID
-		limit  uint64
-		offset uint64
-	}
+// 	type args struct {
+// 		ctx    context.Context
+// 		userID uuid.UUID
+// 		limit  uint64
+// 		offset uint64
+// 	}
 
-	tests := []struct {
-		name    string
-		fields  fields
-		args    args
-		want    []*entity.Project
-		wantErr bool
-	}{
-		{
-			name: "success",
-			args: args{
-				ctx:    context.Background(),
-				userID: uuid.New(),
-			},
-			want:    []*entity.Project{},
-			wantErr: false,
-		},
-		{
-			name: "error",
-			args: args{
-				ctx:    context.Background(),
-				userID: uuid.Nil,
-			},
-			want:    nil,
-			wantErr: true,
-		},
-	}
+// 	tests := []struct {
+// 		name    string
+// 		fields  fields
+// 		args    args
+// 		want    []*entity.Project
+// 		wantErr bool
+// 	}{
+// 		{
+// 			name: "success",
+// 			args: args{
+// 				ctx:    context.Background(),
+// 				userID: uuid.New(),
+// 			},
+// 			want:    []*entity.Project{},
+// 			wantErr: false,
+// 		},
+// 		{
+// 			name: "error",
+// 			args: args{
+// 				ctx:    context.Background(),
+// 				userID: uuid.Nil,
+// 			},
+// 			want:    nil,
+// 			wantErr: true,
+// 		},
+// 	}
 
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			log := slog.New(slog.NewTextHandler(os.Stdout, nil))
-			mockStorage := mocks.NewMockStorage(t)
+// 	for _, tt := range tests {
+// 		t.Run(tt.name, func(t *testing.T) {
+// 			log := slog.New(slog.NewTextHandler(os.Stdout, nil))
+// 			mockStorage := mocks.NewMockStorage(t)
 
-			mockStorage.On("GetProjects", tt.args.ctx, tt.args.userID, tt.args.limit, tt.args.offset).Maybe().Return(tt.want, nil)
+// 			mockStorage.On("GetProjects", tt.args.ctx, tt.args.userID, tt.args.limit, tt.args.offset).Maybe().Return(tt.want, nil)
 
-			u := &usecase.UseCase{
-				Log:    log,
-				DB:     mockStorage,
-			}
+// 			u := &usecase.UseCase{
+// 				Log:    log,
+// 				DB:     mockStorage,
+// 			}
 
-			got, err := u.GetProjects(tt.args.ctx, tt.args.userID, tt.args.limit, tt.args.offset)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("GetProjects() error = %v, wantErr %v", err, tt.wantErr)
-			}
+// 			got, err := u.GetProjects(tt.args.ctx, tt.args.userID, tt.args.limit, tt.args.offset)
+// 			if (err != nil) != tt.wantErr {
+// 				t.Errorf("GetProjects() error = %v, wantErr %v", err, tt.wantErr)
+// 			}
 
-			if tt.want != nil {
-				require.Equal(t, tt.want, got)
-			}
+// 			if tt.want != nil {
+// 				require.Equal(t, tt.want, got)
+// 			}
 
-			if tt.wantErr {
-				require.Error(t, err)
-			}
-		})
-	}
-}
+// 			if tt.wantErr {
+// 				require.Error(t, err)
+// 			}
+// 		})
+// 	}
+// }
